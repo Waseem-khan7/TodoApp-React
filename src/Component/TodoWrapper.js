021708acea016497df35c3b0c38908a8c575a00f
Waseem-khan7 <waseemnasirkhan24@gmail.com>
2023-10-12 14:58:13 +0530

    Initial commit

--- src/Component/TodoWrapper.js
+++ src/Component/TodoWrapper.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { Todo } from "./Todo";
 import { TodoForm } from "./TodoForm";
 import { v4 as uuidv4 } from "uuid";
-import { EditTodoForm } from "./EditTodoForm";
 
 export const TodoWrapper = () => {
   const [todos, setTodos] = useState([]);
@@ -10,35 +9,13 @@ export const TodoWrapper = () => {
   const addTodo = (todo) => {
     setTodos([
       ...todos,
-      { id: uuidv4(), task: todo, completed: false, isEditing: false },
+      { id: uuidv4(), task: todo },
     ]);
   }
 
   const deleteTodo = (id) => setTodos(todos.filter((todo) => todo.id !== id));
 
-  const toggleComplete = (id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      )
-    );
-  }
 
-  const editTodo = (id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
-      )
-    );
-  }
-
-  const editTask = (task, id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
-      )
-    );
-  };
 
   return (
     <div className="TodoWrapper">
@@ -46,15 +23,11 @@ export const TodoWrapper = () => {
       <TodoForm addTodo={addTodo} />
       {/* display todos */}
       {todos.map((todo) =>
-        todo.isEditing ? (
-          <EditTodoForm editTodo={editTask} task={todo} />
-        ) : (
+        (
           <Todo
             key={todo.id}
             task={todo}
             deleteTodo={deleteTodo}
-            editTodo={editTodo}
-            toggleComplete={toggleComplete}
           />
         )
       )}
